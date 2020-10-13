0d268be5323f3c83cb172b44ea15e879fc6d71ed
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2020-10-12 23:28:30 -0300

    AUla1_final

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,16 @@
 
 fun main(){
-    print("Bem vindo ao Bytebank!");
+    println("Bem vindo ao Bytebank!");
+
+    var titular = "Giuseppe"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+
+    saldo += 200
+
+    println("O titular eh: $titular")
+    println("A conta eh: $numeroConta")
+    println("O saldo eh: $saldo")
+
 }
